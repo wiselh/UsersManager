9fb0aff5277e22f086893aea1327e780986f1b36
wiselh <wise.self.2@gmail.com>
2018-04-22 15:53:44 +0100

    Generate Login - Register pages & add it to the module
    $ ionic g page login
    $ ionic g page register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+//added
+import { LoginPage } from './../pages/login/login';
+import { RegisterPage } from './../pages/register/register';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
